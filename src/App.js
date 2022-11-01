d2cf68e235c90c327904b9f9bb7c2f9ea462e460
Ravi Thakor <raviak47rv3kr@gmail.com>
2022-11-01 16:33:28 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { useEffect, useState } from "react";
+
+const defaultOptions = [
+  { value: "apple", label: "Apple" },
+  { value: "burger", label: "Burger" },
+  { value: "juice", label: "Juice" },
+  { value: "frankie", label: "Frankie" },
+  { value: "soup", label: "Soup" },
+];
 
 function App() {
+  const [mondayValue, setMondayValue] = useState("");
+  const [tuesdayValue, setTuesdayValue] = useState("");
+  const [wednesdayValue, setWednesdayValue] = useState("");
+
+  const [options, setOptions] = useState(defaultOptions);
+
+  useEffect(() => {
+    if (mondayValue || tuesdayValue || wednesdayValue) {
+      const filteredArray = defaultOptions.filter(
+        (item) =>
+          !(
+            item.label === mondayValue ||
+            item.label === tuesdayValue ||
+            item.label === wednesdayValue
+          )
+      );
+      setOptions([...filteredArray]);
+    }
+  }, [mondayValue, tuesdayValue, wednesdayValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="MainContainer">
+      <div className="DropdownDiv">
+        <label className="DropdownTitle">Monday</label>
+        <Dropdown
+          className="DropdownStyle"
+          options={options}
+          value={mondayValue}
+          placeholder="Select an option"
+          onChange={(e) => setMondayValue(e.label)}
+        />
+      </div>
+      <div className="DropdownDiv">
+        <label className="DropdownTitle">Tuesday</label>
+        <Dropdown
+          className="DropdownStyle"
+          options={options}
+          value={tuesdayValue}
+          onChange={(e) => setTuesdayValue(e.label)}
+          placeholder="Select an option"
+        />
+      </div>
+      <div className="DropdownDiv">
+        <label className="DropdownTitle">Wednesday</label>
+        <Dropdown
+          className="DropdownStyle"
+          options={options}
+          value={wednesdayValue}
+          onChange={(e) => setWednesdayValue(e.label)}
+          placeholder="Select an option"
+        />
+      </div>
     </div>
   );
 }
